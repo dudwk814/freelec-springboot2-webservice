c44ca25f0018cf5149efcd1131ffded8ce75c967
dudwk814 <kj99658103@gmail.com>
2020-09-05 10:41:48 +0900

    2020-09-05 spring security test

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//JAP Auditing 기능 활성화
-@EnableJpaAuditing
+// @EnableJpaAuditing 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
