3b1acc915ebb665a2b127fa66bd3ebafe722ebee
dudwk814 <kj99658103@gmail.com>
2020-09-07 23:38:05 +0900

    2020-09-07 nginx

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,11 +8,11 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
-echo  "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
@@ -25,7 +25,7 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
